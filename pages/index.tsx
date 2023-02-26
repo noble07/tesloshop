8be74c3e071b6fb3f8e7df9b3591b9e503afefe7
Juan Camilo Munoz Zuleta <juancmz98@gmail.com>
2023-02-25 19:18:55 -0500

    Added base home layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,11 @@
+import { ShopLayout } from "@/components/layouts";
+import { Typography } from "@mui/material";
+
 export default function Home() {
   return (
-    <h1>Telos-shop</h1>
+    <ShopLayout title="Teslo-Shop - Home" pageDescription="Encuentra los mejores productos de Teslo aquí">
+      <Typography variant="h1" component="h1">Tienda</Typography>
+      <Typography variant="h2" sx={{ mb: 1 }}>Todos los productos</Typography>
+    </ShopLayout>
   )
 }
