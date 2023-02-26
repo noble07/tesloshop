e6523176b62c46e97d73e55d7db1317b0813e19a
Juan Camilo Munoz Zuleta <juancmz98@gmail.com>
2023-02-25 21:46:19 -0500

    Added demo data to home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,33 @@
-import { ShopLayout } from "@/components/layouts";
-import { Typography } from "@mui/material";
+import { ShopLayout } from '@/components/layouts'
+import { Card, CardActionArea, CardMedia, Grid, Typography } from '@mui/material'
+
+import { initialData } from '@/database/products'
 
 export default function Home() {
   return (
     <ShopLayout title="Teslo-Shop - Home" pageDescription="Encuentra los mejores productos de Teslo aquí">
       <Typography variant="h1" component="h1">Tienda</Typography>
       <Typography variant="h2" sx={{ mb: 1 }}>Todos los productos</Typography>
+
+      <Grid container spacing={4}>
+        {
+          initialData.products.map(product => (
+            <Grid item xs={6} sm={4} key={product.slug}>
+              <Card>
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    image={`products/${product.images[0]}`}
+                    alt={product.title}
+                  >
+                  </CardMedia>
+                </CardActionArea>
+              </Card>
+            </Grid>
+          ))
+        }
+      </Grid>
+      
     </ShopLayout>
   )
 }
