dc4e75d749da7d4649395cbe18fb9cc55fe8bde9
Juan Camilo Munoz Zuleta <juancmz98@gmail.com>
2023-02-25 20:48:40 -0500

    Added custom 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,24 @@
+import { ShopLayout } from "@/components/layouts";
+import { Box, Typography } from "@mui/material";
+
+export default function Custom404() {
+  return (
+    <ShopLayout title="Page not found" pageDescription="No hay nada que mostrar aquí">
+        <Box
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          height="calc(100vh - 200px)"
+          sx={{ 
+            flexDirection: { 
+              xs: 'column', 
+              md: 'row' 
+            }
+          }}
+        >
+          <Typography variant="h1" component="h1" fontSize={80} fontWeight={200}>404 |</Typography>
+          <Typography marginLeft={2}>No encontramos ninguna página aquí.</Typography>
+        </Box>
+    </ShopLayout>
+  )
+}
