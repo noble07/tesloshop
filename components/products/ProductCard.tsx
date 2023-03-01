513121ecd05d675cb3e89ea2050d4d68b89eab7e
Juan Camilo Munoz Zuleta <juancmz98@gmail.com>
2023-02-28 20:41:24 -0500

    Added product price, name and second image to Product Card

--- components/products/ProductCard.tsx
+++ components/products/ProductCard.tsx
@@ -1,23 +1,45 @@
+import { useMemo, useState } from 'react'
 import { IProduct } from '@/interfaces'
-import { Grid, Card, CardActionArea, CardMedia } from '@mui/material'
+import { Grid, Card, CardActionArea, CardMedia, Box, Typography } from '@mui/material'
 
 interface ProductCardProps {
   product: IProduct
 }
 
 const ProductCard = ({product}: ProductCardProps) => {
+
+  const [isHovered, setIsHovered] = useState(false)
+
+  const productImage = useMemo(() => {
+    return isHovered
+      ? `products/${product.images[1]}`
+      : `products/${product.images[0]}`
+  }, [isHovered, product.images])
+  
   return (
-    <Grid item xs={6} sm={4} key={product.slug}>
+    <Grid
+      item
+      xs={6}
+      sm={4}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
       <Card>
         <CardActionArea>
           <CardMedia
             component="img"
-            image={`products/${product.images[0]}`}
+            className="fadeIn"
+            image={productImage}
             alt={product.title}
           >
           </CardMedia>
         </CardActionArea>
       </Card>
+
+      <Box sx={{mt: 1}} className="fadeIn">
+        <Typography fontWeight={700}>{product.title}</Typography>
+        <Typography fontWeight={500}>${product.price}</Typography>
+      </Box>
     </Grid>
   )
 }
