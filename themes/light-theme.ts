a9e7a724160fc48f8f2227e447111a2b06be8fac
Juan Camilo Munoz Zuleta <juancmz98@gmail.com>
2023-02-28 21:39:35 -0500

    Fix Mui Button styles

--- themes/light-theme.ts
+++ themes/light-theme.ts
@@ -8,6 +8,9 @@ export const lightTheme = createTheme({
     },
     secondary: {
       main: '#3A64D8'
+    },
+    info: {
+      main: '#FFF'
     }
   },
   components: {
@@ -51,11 +54,10 @@ export const lightTheme = createTheme({
         variant: 'contained',
         size: 'small',
         disableElevation: true,
+        color: 'info'
       },
       styleOverrides: {
         root: {
-          backgroundColor: 'white',
-          color: 'black',
           textTransform: 'none',
           boxShadow: 'none',
           borderRadius: 10,
