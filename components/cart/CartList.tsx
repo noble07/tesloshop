6de284c669ab898448678ef14d4c71375df98836
Juan Camilo Munoz Zuleta <juancmz98@gmail.com>
2023-03-04 12:44:55 -0500

    Added CartList component

--- components/cart/CartList.tsx
+++ components/cart/CartList.tsx
@@ -1,5 +1,7 @@
+import NextLink from 'next/link'
 import { initialData } from '@/database/products'
-import { Typography } from '@mui/material'
+import { Box, Button, CardActionArea, CardMedia, Grid, Link, Typography } from '@mui/material'
+import { ItemCounter } from '../ui'
 
 const productsInCart = [
   initialData.products[0],
@@ -12,7 +14,38 @@ const CartList = () => {
     <>
       {
         productsInCart.map(product => (
-          <Typography key={product.slug}>{product.title}</Typography>
+          <Grid key={product.slug} container spacing={2} sx={{ mb: 1 }}>
+            <Grid item xs={3}>
+              {/* TODO: llevar a la página del producto */}
+              <NextLink legacyBehavior href="/product/slug" passHref>
+                <Link>
+                  <CardActionArea>
+                    <CardMedia
+                      image={`products/${product.images[0]}`}
+                      component="img"
+                      sx={{ borderRadius: '5px' }}
+                    />
+                  </CardActionArea>
+                </Link>
+              </NextLink>
+            </Grid>
+            <Grid item xs={7}>
+              <Box display="flex" flexDirection="column">
+                <Typography variant="body1">{product.title}</Typography>
+                <Typography variant="body1">Talla: <strong>M</strong></Typography>
+
+                {/* Condicional */}
+                <ItemCounter />
+              </Box>
+            </Grid>
+            <Grid item xs={2} display="flex" alignItems="center" flexDirection="column">
+              <Typography variant="subtitle1">${product.price}</Typography>
+              {/* Editable */}
+              <Button variant="text" color="secondary">
+                Remover
+              </Button>
+            </Grid>
+          </Grid>
         ))
       }
     </>
