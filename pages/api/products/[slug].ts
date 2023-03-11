ebed5baa21991d0c91032edc77f166ccf89c38c2
Juan Camilo Muñoz Zuleta <juancmz98@gmail.com>
2023-03-10 23:58:55 -0500

    Added find product by slug endpoint

---
+++ pages/api/products/[slug].ts
@@ -0,0 +1,34 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+import { IProduct } from '@/interfaces'
+import { db } from '@/database'
+import { Product } from '@/models'
+
+type Data = 
+| { message: string }
+| IProduct
+
+export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+  switch (req.method) {
+    case 'GET':
+      return getProductBySlug(req, res)
+    default:
+      return res.status(400).json({
+        message: 'Bad Request'
+      })
+  }
+}
+
+const getProductBySlug = async(req: NextApiRequest, res: NextApiResponse<Data>) => {
+  const { slug } = req.query
+
+  await db.connect()
+
+  const product = await Product.findOne({ slug }).lean()
+  
+  await db.disconnect()
+
+  if (!product) return res.status(404).json({ message: 'No hay un producto con ese slug' })
+
+  return res.status(200).json(product)
+}
