9b57a865a5ec0e5511cd12a6f911fb5cb33dae37
Juan Camilo Muñoz Zuleta <juancmz98@gmail.com>
2023-03-15 21:38:12 -0500

    Added swr fetch on home products

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,15 +3,25 @@ import { Typography } from '@mui/material'
 import { ShopLayout } from '@/components/layouts'
 import { ProductList } from '@/components/products'
 
-import { initialData } from '@/database/products'
+import useSWR from 'swr'
 
-export default function Home() {
+const fetcher = (...args: [key: string]) => fetch(...args).then(res => res.json())
+
+export default function HomePage() {
+
+  const { data, error, isLoading } = useSWR('/api/products', fetcher)
+
+  if (error) return <div>failed to load</div>
+  if (isLoading) return <div>loading...</div>
+
+  console.log({data})
+
   return (
     <ShopLayout title="Teslo-Shop - Home" pageDescription="Encuentra los mejores productos de Teslo aquí">
       <Typography variant="h1" component="h1">Tienda</Typography>
       <Typography variant="h2" sx={{ mb: 1 }}>Todos los productos</Typography>
 
-      <ProductList products={initialData.products as any} />
+      <ProductList products={data} />
       
     </ShopLayout>
   )
