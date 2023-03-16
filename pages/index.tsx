bc6ed2587c812731c0a5b229ae684a0b00e18181
Juan Camilo Muñoz Zuleta <juancmz98@gmail.com>
2023-03-15 21:54:53 -0500

    Added useProducts custom hook

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,27 +2,23 @@ import { Typography } from '@mui/material'
 
 import { ShopLayout } from '@/components/layouts'
 import { ProductList } from '@/components/products'
-
-import useSWR from 'swr'
-
-const fetcher = (...args: [key: string]) => fetch(...args).then(res => res.json())
+import { useProducts } from '@/hooks'
 
 export default function HomePage() {
 
-  const { data, error, isLoading } = useSWR('/api/products', fetcher)
-
-  if (error) return <div>failed to load</div>
-  if (isLoading) return <div>loading...</div>
-
-  console.log({data})
+  const {products, isLoading, isError} = useProducts('/products')
 
   return (
     <ShopLayout title="Teslo-Shop - Home" pageDescription="Encuentra los mejores productos de Teslo aquí">
       <Typography variant="h1" component="h1">Tienda</Typography>
       <Typography variant="h2" sx={{ mb: 1 }}>Todos los productos</Typography>
 
-      <ProductList products={data} />
-      
+      {
+        isLoading
+          ? <h1>Cargando...</h1>
+          : <ProductList products={products} />
+      }
+
     </ShopLayout>
   )
 }
