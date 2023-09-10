2611a5e5d3345011a7ed40a2cf0f3f42e4a811fd
Juan Camilo Muñoz Zuleta <juancmz98@gmail.com>
2023-09-10 16:31:05 -0500

    Load products from cookie

--- context/cart/CartProvider.tsx
+++ context/cart/CartProvider.tsx
@@ -1,4 +1,5 @@
-import { useReducer } from 'react'
+import { useEffect, useReducer } from 'react'
+import Cookie from 'js-cookie'
 import { ICartProduct } from '@/interfaces'
 import { CartContext, cartReducer } from './'
 
@@ -15,7 +16,21 @@ const CART_INITIAL_STATE: CartState = {
 }
 
 export const CartProvider = ({ children }: CartProviderProps) => {
+
   const [state, dispatch] = useReducer(cartReducer, CART_INITIAL_STATE)
+
+  useEffect(() => {
+    try {
+      const cookieProducts = Cookie.get('cart') ? JSON.parse(Cookie.get('cart')!) : []
+      dispatch({ type: '[Cart] - LoadCart from cookies | storage', payload: cookieProducts })
+    } catch (error) {
+      dispatch({ type: '[Cart] - LoadCart from cookies | storage', payload: [] })
+    }
+  }, [])
+
+  useEffect(() => {
+    if (state.cart.length > 0) Cookie.set('cart', JSON.stringify(state.cart))
+  }, [state.cart])
 
   const addProductToCart = (newProduct: ICartProduct) => {
     const currentProduct = state.cart.find(product =>
